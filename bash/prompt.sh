d10ba84c53a726cf5deae5aa8e1a00cd383dc0dc
Andre Azevedo <andre.azevedo@gmail.com>
2014-10-21 00:39:16 -0400

    Better organization of files

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,6 +1,6 @@
-##########
-# PROMPT #
-##########
+###############################
+# GIT, MERCUARIAL AND COLORS  #
+###############################
 __current_branch()
 {
   # find out if we're in a git or hg repo by looking for the control dir
@@ -68,10 +68,3 @@ customize_prompt()
 
 customize_prompt
 
-
-#############
-# UTILITIES #
-#############
-# LS
-alias ls='ls --color'
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
---
+++ bash/utilities.sh
@@ -0,0 +1,6 @@
+######
+# LS #
+######
+alias ls='ls --color=auto'
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+
