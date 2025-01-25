3128e50df57bbc134b0bd750ca8a452e16c66372
Andre Azevedo <andre.azevedo@gmail.com>
2025-01-25 20:03:22 +0000

    Remove unecessary white-spaces

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,16 +5,16 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all" (supported parsers)
             ensure_installed = { "lua", "vim", "vimdoc", "javascript", "python", "c" },
-            
+
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-            
+
             -- Automatically install missing parsers when entering buffer
             auto_install = true,
-            
+
             highlight = {
                 enable = true,
-                
+
                 -- Or use a function for more flexibility
                 additional_vim_regex_highlighting = false,
             },
